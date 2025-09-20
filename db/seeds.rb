75780be7e4f0e78725845d8153403b1261d063d2
topi0247 <karubona.ra247@gmail.com>
2025-09-20 22:07:08 +0900

    fix: rubocop修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,2 @@
-
 env_seed_file = Rails.root.join("db", "seeds", "#{Rails.env.downcase}.rb")
 load env_seed_file if File.exist?(env_seed_file)
--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -3,7 +3,6 @@ user = User.find_or_create_by!(email: "user1@example.com") do |u|
   u.password = "password"
 end
 
-
 statuses = ["To do", "Progress", "Done"]
 statuses.each do |s|
   Status.find_or_create_by!(name: s)
@@ -11,7 +10,10 @@ end
 
 15.times do |i|
   Todo.find_or_create_by!(title: "タイトル_#{i}_タイトルタイトルタイトルタイトル") do |t|
-    t.body = "タスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入る"
+    t.body = "タスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入る" \
+             "タスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入る" \
+             "タスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入るタスクの詳細文章が入る" \
+             "タスクの詳細文章が入る"
     t.user = user
     t.status = Status.all.sample
   end
