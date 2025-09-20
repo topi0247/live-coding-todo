7b0922d1fe5b925847ac7ea26493562619ef189d
topi0247 <karubona.ra247@gmail.com>
2025-09-20 19:51:19 +0900

    feat: ログアウト実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  def destroy
+    sign_out(resource_name)
+    redirect_to new_user_session_path, success: "ログアウトしました"
+  end
 end
