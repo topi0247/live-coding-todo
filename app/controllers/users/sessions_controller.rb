1fe41366de6857593106b8415eb76872d5b6e441
topi0247 <karubona.ra247@gmail.com>
2025-09-20 20:16:18 +0900

    feat: ログイン実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  def create
+    self.resource = warden.authenticate(auth_options)
+
+    if resource
+      flash[:success] = "ログインに成功しました"
+      sign_in(resource_name, resource)
+      redirect_to root_path
+    else
+      flash.now[:error] = "ログインに失敗しました"
+      self.resource = resource_class.new
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     sign_out(resource_name)
     redirect_to new_user_session_path, success: "ログアウトしました"
   end
+
+
 end
